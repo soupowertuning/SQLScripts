4f84950d9de37f3b19740cd5982c948e713bceba
Rodrigo Ribeiro Gomes <rodrigor.gomes@hotmail.com>
2023-07-07 12:23:48 -0300

    Create ContainedAgLoginsCheck.sql

---
+++ alwayson/ContainedAgLoginsCheck.sql
@@ -0,0 +1,6 @@
+/*
+  Este script foi criado como uma POC no vídeo da PowerTuning sobre Contained Availability Groups
+  Ele usa o linkd server para conectar no ambiente de cada AG e exibe em quais ambientes um login existe ou não existe
+*/
+
+
